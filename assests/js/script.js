32bd5779c6983cd0a5f9bddc02f080c0e7274a82
PeterM012 <110750833+PeterM012@users.noreply.github.com>
2022-10-05 20:48:50 -0700

    add local storage for text area

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,44 +1,39 @@
 var setDate = moment().format('dddd') + " " + moment().format("MMM Do YYYY");
-var setHour = moment().format("h:mm:ss a")
-
 var hour = moment().hours();
 
 var interval = setInterval(function() {
     var now = moment();
-    $("#currentDay").html(now.format("MMM Do YYYY")) + "" + now.format("dddd");
+    $("#currentDay").html(setDate);
     $("#currentDay").html(setDate + "" + now.format(" h:mm:ss a"));
-    for (let i = 9; i <= 18; i++) {
-    var event = document.getElementById(i + "")
-         if (i > now.hours()) { //future
-            event.classList.add("future");
-        } 
-        else if(i < now.hours()) { //past
-            event.classList.add("past");
-        }
-        else { event.classList.add("present");//present
-        }
-}}, 100);
+    hourColor();
+}, 100);
 
-// function stringifyLocalStorage(){
-//     var stringifyObjects = JSON.parse(localStorage.getItem());
-//     for (var i = 9; i < 18; i++)
-//  }
-
-// function parseLocalStorage(){
-//     var parsedObjects = [];
-//     for (var i = 9; i < 18; i++) {
-//         parsedObjects.push(JSON.parse(localStorage.getItem()));
-//     }
-//     return parsedObjects;
-// }
+function hourColor() {
+    for (let i = 9; i < 18; i++) {
+        var event = document.getElementById(i + "")
+             if (i > hour) { //future
+                event.classList.add("future");
+            } 
+            else if(i < hour) { //past
+                event.classList.add("past");
+                if (i == 17) {
+                    break;
+                }
+            }
+            else { event.classList.add("present");//present
+            }
+        };
+}
 
     for (let i = 9; i < 18; i++) {
-        $("#" + i).val(localStorage.getItem(JSON.parse(i)));  
+        var read = localStorage.getItem("" + i);
+        if(read !== null) {
+            $("#" + i).val(read);
+        }
     }
 
-
 $(".saveBtn").on("click", function(){
-    localStorage.setItem($(this).siblings("textarea").attr("id"),JSON.stringify($(this).siblings("textarea").val()))
+    localStorage.setItem($(this).siblings("textarea").attr("id"), $(this).siblings("textarea").val());
 }) 
 
 
