2d3de9ef911281f9781fca1e380334d7264f4e28
PeterM012 <110750833+PeterM012@users.noreply.github.com>
2022-10-06 19:37:33 -0700

    add comments to js

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,6 +1,8 @@
+// Time in the jumbotron showing current day and time 
 var setDate = moment().format('dddd') + " " + moment().format("MMM Do YYYY");
 var hour = moment().hours();
 
+// time interval for keeping day and time updated at real time
 var interval = setInterval(function() {
     var now = moment();
     $("#currentDay").html(setDate);
@@ -8,6 +10,8 @@ var interval = setInterval(function() {
     hourColor();
 }, 100);
 
+//Function with a for loop that loops through the my array of numbers. 
+//Displays the color needed depending on the time of day
 function hourColor() {
     for (let i = 9; i < 18; i++) {
         var event = document.getElementById(i + "")
@@ -24,14 +28,14 @@ function hourColor() {
             }
         };
 }
-
+//for loop for my local storage to retrieve the items saved 
     for (let i = 9; i < 18; i++) {
         var read = localStorage.getItem("" + i);
         if(read !== null) {
             $("#" + i).val(read);
         }
     }
-
+//click function that saves the information in the textarea when the save button is clicked
 $(".saveBtn").on("click", function(){
     localStorage.setItem($(this).siblings("textarea").attr("id"), $(this).siblings("textarea").val());
 }) 
